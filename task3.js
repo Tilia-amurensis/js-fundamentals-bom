6a0ae504ed2a8aef86954a3027bf1e1a044f3a6a
Alina Kolomiitseva <alina_kolomiitseva@epam.com>
2025-01-04 23:30:31 +0200

    Added: solved task3.js

--- task3.js
+++ task3.js
@@ -1,5 +1,18 @@
-// Реалізуйте програму, яка по натисканню на кнопку видалятиме обраний елемент випадаючого списку. 
+// Реалізуйте програму, яка по натисканню на кнопку видалятиме обраний елемент випадаючого списку.
 // Можуть видалятися всі елементи в будь-якому порядку.
 // Список з елементами знаходиться у файлі index.html
 // Для запуску використовувати Live Server
 // Для перевірки виконання виконати команду node tests/task3.test.js
+
+const dropdown = document.getElementById("dropdown");
+const deleteButton = document.getElementById("deleteButton");
+function deleteElem(event) {
+  const index = dropdown.selectedIndex;
+
+  if (index !== -1) {
+    dropdown.options[index].remove();
+  } else {
+    alert("Please select an option to remove!");
+  }
+}
+deleteButton.addEventListener("click", deleteElem);
