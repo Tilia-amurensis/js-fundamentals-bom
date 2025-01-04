d3afc5bd27fad9aaceb75923c81385b7fb6a2a61
Alina Kolomiitseva <alina_kolomiitseva@epam.com>
2025-01-05 00:08:33 +0200

    Added: solved task4.js

--- task4.js
+++ task4.js
@@ -2,3 +2,7 @@
 //  і виводитиме на поточну сторінку при її розтязі/стисканні відповідні значення.
 // Для запуску використовувати Live Server
 // Для перевірки node tests/task4.test.js
+const browserWidth = document.getElementById("widthValue");
+const browserHeight = document.getElementById("heightValue");
+browserWidth.innerHTML =  document.documentElement.clientWidth;
+browserHeight.innerHTML = document.documentElement.clientHeight;
