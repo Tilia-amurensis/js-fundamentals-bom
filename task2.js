0452c3cb05d50b66b79a8b4c39f9157686c98ea8
Alina Kolomiitseva <alina_kolomiitseva@epam.com>
2025-01-04 20:17:05 +0200

    Added: solved task2.js

--- task2.js
+++ task2.js
@@ -5,3 +5,11 @@
 //         
 // Запустіть виконане завдання за допомогою Live Server 
 // Перевірте виконання за допомогою команди node tests/task2.test.js
+
+const btn1 = document.getElementById("button1");
+const btn2 = document.getElementById("button2");
+const btn3 = document.getElementById("button3");
+
+btn1.addEventListener("click", () => btn1.style.backgroundColor = "blue");
+btn2.addEventListener("dblclick", () => btn2.style.backgroundColor = "pink");
+btn3.addEventListener("mouseover", () => btn3.style.backgroundColor = "brown");
