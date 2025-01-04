3c7bed24defc4e4ec32093ae329b86075b81353f
Alina Kolomiitseva <alina_kolomiitseva@epam.com>
2025-01-04 19:44:27 +0200

    Added: solved task1.js

--- task1.js
+++ task1.js
@@ -4,4 +4,11 @@
 //  і змінюватиме стиль вмісту тега <p>: колір шрифту – оранжевий, розмір шрифту 20px, шрифт сімейства "Comic Sans MS".
 // Запустіть програму за допомогою Live Server
 // перевірте виконання за допомогою команди node tests/task1.test.js
-
+const btn = document.querySelector("#btn");
+const p = document.querySelector("#text");
+function changeCSS(event) {
+ p.style.color = "orange";
+ p.style.fontSize = "20px";
+ p.style.fontFamily = "Comic Sans MS";
+}
+btn.addEventListener("click", changeCSS);
